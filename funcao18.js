7f77daa169a7f6030f695f293dc76b7a2737ee59
Pedro Neves <pedro.t.dasneves@gmail.com>
2022-10-31 21:49:24 -0300

    Fazer o login

--- funcao18.js
+++ funcao18.js
@@ -1,14 +1,26 @@
 var nomes = []
 var senhas = []
-var index = 0 
+var index = 0
 
 function CadastrarUsuario() {
-     nomes{index} = prompt("Insira o nome para cadastro")
-     senhas{index} = prompt("Crie sua senha para cadastro")
+     nomes[index] = prompt("Insira o nome para cadastro")
+     senhas[index] = prompt("Crie sua senha para cadastro")
      index++
 }
 
 function EscolherOpcao() {
      var opcao = prompt("Insira a função de cadastro desejad. 1 = cadastro; 2 = login; 3 = excluir cadastro; 4 = encerrar")
      return opcao
+}
+
+function FazerLogin(nome, senha) {
+     for (var contador = 0; contador < nomes.length; contador++) {
+          if (nome == nome[contador]) {
+               if (senha == senhas[contador]) {
+                    return true 
+               }
+          }
+     }
+     return false
+
 }
