5497aa3e70de44b482b75a16b4007bf73fb19e95
Pedro Neves <pedro.t.dasneves@gmail.com>
2022-10-31 21:39:57 -0300

    Escolha de Opção

--- funcao18.js
+++ funcao18.js
@@ -7,3 +7,8 @@ function CadastrarUsuario() {
      senhas{index} = prompt("Crie sua senha para cadastro")
      index++
 }
+
+function EscolherOpcao() {
+     var opcao = prompt("Insira a função de cadastro desejad. 1 = cadastro; 2 = login; 3 = excluir cadastro; 4 = encerrar")
+     return opcao
+}
